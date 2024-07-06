786646bdd72785d3dbadb8b4d1607386c77a2693
Gianfranco <barriosgianfranco@gmail.com>
2024-07-05 23:03:47 -0600

    Login y registro de nuevos usuarios funcionando correctamente

--- Economica/BD/datos.js
+++ Economica/BD/datos.js
@@ -1,3 +1,15 @@
-export const users = ['gianfranco'];
-export const password = ['2424'];
-export const frutas = ['manzana', 'pera', 'sandia'];
+const users = JSON.parse(localStorage.getItem('users')) || ["gianfranco"];
+const password = JSON.parse(localStorage.getItem('password')) || ["2424"];
+const frutas =["sandias"];
+
+export {users, password };
+
+
+export function agregarUsuario(nuevoUsuario){
+    users.push(nuevoUsuario);
+    localStorage.setItem('users', JSON.stringify(users));
+}
+export function agregarContraseña(nuevaContraseña){
+    password.push(nuevaContraseña);
+    localStorage.setItem('password', JSON.stringify(password));
+}
--- Economica/index.js
+++ Economica/index.js
@@ -1,17 +1,25 @@
-import { users } from "./BD/datos.js";
-import { password } from "./BD/datos.js";
+import { users, password } from "../BD/datos.js";
 
+console.log("Usuarios:", users);
+console.log("Contraseñas:", password);
 document.getElementById("loginForm").addEventListener("submit", function(event){
     event.preventDefault();
 
     let usuario = document.getElementById("usuario").value;
     let contraseña = document.getElementById("contraseña").value;
+    let usuarioEncontrado = false;
+    for (let i = 0; i<users.length; i++){
+        if (users[i] == usuario && password[i] == contraseña){
+            usuarioEncontrado = true;
 
-    if (usuario == users && contraseña == password){
-        window.location.href = ".\\frutas\\display.html";
-    }else {
-        alert ("Usuario y o contraseña incorrectos");
+        }
     }
+    if (usuarioEncontrado) {
+        window.location.href=".\\frutas\\display.html";
+    }else {
+        alert("Usuario y/o Contraseña incorrectos ")
+    }
+
 })
 
 document.getElementById("registrarse").addEventListener("click", function(){
