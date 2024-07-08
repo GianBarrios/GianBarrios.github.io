f5e4c2b92cb33ce43261683500991e73b33be844
Gianfranco <barriosgianfranco@gmail.com>
2024-07-07 20:09:44 -0600

    configuracion del display

--- Economica/frutas/display.js
+++ Economica/frutas/display.js
@@ -4,3 +4,17 @@ const toggleBtn = document.querySelector('.toggle-btn');
 toggleBtn.addEventListener('click', () => {
     sidebar.classList.toggle('active');
 })
+
+document.getElementById('agregar').addEventListener('click', function(event){
+    event.preventDefault();
+    cargarContenido('..\\files\\agregar.html')
+})
+
+function cargarContenido (url) {
+    fetch(url)
+    .then(response => response.text())
+    .then(data => {
+        document.getElementById('agregar').innerHTML =data;
+    })
+    .catch (error => console.error ('error al cargar el contenido: ',error))
+}
